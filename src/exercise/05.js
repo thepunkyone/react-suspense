8f684af0fb8af422e09d649679e3f21455206be7
Whitney Bluzma <thepunkyone@gmail.com>
2024-02-08 11:26:38 +0000

    feat: exercise 7 - suspend an image component via custom component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -8,6 +8,7 @@ import {
   PokemonForm,
   PokemonDataView,
   PokemonErrorBoundary,
+  getImageUrlForPokemon,
 } from '../pokemon'
 import {createResource} from '../utils'
 
@@ -20,22 +21,44 @@ import {createResource} from '../utils'
 // we need to make a place to store the resources outside of render so
 // 🐨 create "cache" object here.
 
-// 🐨 create an Img component that renders a regular <img /> and accepts a src
-// prop and forwards on any remaining props.
-// 🐨 The first thing you do in this component is check whether your
-// imgSrcResourceCache already has a resource for the given src prop. If it does
-// not, then you need to create one (💰 using createResource).
-// 🐨 Once you have the resource, then render the <img />.
-// 💰 Here's what rendering the <img /> should look like:
-// <img src={imgSrcResource.read()} {...props} />
+const imgSrcResourceCache = {}
+
+function preloadImage(src) {
+  return new Promise(resolve => {
+    const img = document.createElement('img')
+    img.src = src
+    img.onload = () => resolve(src)
+  })
+}
+
+function createImgSrcResource(src) {
+  return createResource(preloadImage(src))
+}
+
+function getImgSrcResource(src) {
+  let imgSrcResource = imgSrcResourceCache[src]
+
+  if (!imgSrcResource) {
+    imgSrcResource = createImgSrcResource(src)
+
+    imgSrcResourceCache[src] = imgSrcResource
+  }
+
+  return imgSrcResource
+}
+
+function Img({src, alt, ...props}) {
+  const resource = getImgSrcResource(src)
+
+  return <img src={resource.read()} alt={alt} {...props} />
+}
 
 function PokemonInfo({pokemonResource}) {
   const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
-        {/* 🐨 swap this img for your new Img component */}
-        <img src={pokemon.image} alt={pokemon.name} />
+        <Img src={pokemon.image} alt={pokemon.name} />
       </div>
       <PokemonDataView pokemon={pokemon} />
     </div>
